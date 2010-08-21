7205a6bd7d910a6819695a4be2f9fc61a1427c7b
Brendan Heywood <brendan.heywood@gmail.com>
2010-08-22 03:45:10 +1000

    got jsdocs running through ant

--- build.sh
+++
@@ -1,4 +0,0 @@
-
-rm -r docs
-java -jar ../../../Scripts/jsdoc-toolkit/jsrun.jar ../../../Scripts/jsdoc-toolkit/app/run.js -a -t=../../../Scripts/jsdoc-toolkit/templates/jsdoc phototopo.js -d=./docs
-
