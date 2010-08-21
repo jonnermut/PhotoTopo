dbe2ba1fec0a2d8404338c65b34f4c5a1eb01ce8
Brendan Heywood <brendanheywood@Brendan-Heywoods-iMac.local>
2010-08-21 21:05:20 +1000

    added output of jsdoc

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-
+rm -r docs
 java -jar ../../../Scripts/jsdoc-toolkit/jsrun.jar ../../../Scripts/jsdoc-toolkit/app/run.js -a -t=../../../Scripts/jsdoc-toolkit/templates/jsdoc phototopo.js -d=./docs
 
